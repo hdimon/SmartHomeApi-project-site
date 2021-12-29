0ff9a932843e69ab32c98a69420a8c98e98df6d4
Dmitry <hldimon@mail.ru>
2021-12-29 21:52:07 +0300

    Update download links.

--- smarthomeapi/src/components/Downloads.js
+++ smarthomeapi/src/components/Downloads.js
@@ -8,7 +8,7 @@ const FeatureList = [
     //Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        <a href="https://github.com/hdimon/SmartHomeApi/releases/download/1.4.0-alpha-8/1.4.0-alpha-8_linux-x64.zip">1.4.0-alpha-8 x64</a>
+        <a href="https://github.com/hdimon/SmartHomeApi/releases/download/1.4.0-alpha-10/1.4.0-alpha-10_linux-x64.zip">1.4.0-alpha-10 x64</a>
       </>
     ),
   },
@@ -17,8 +17,8 @@ const FeatureList = [
     //Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-          <div><a href="https://github.com/hdimon/SmartHomeApi/releases/download/1.4.0-alpha-8/1.4.0-alpha-8_win-x64.zip">1.4.0-alpha-8 x64</a></div>
-          <div><div><a href="https://github.com/hdimon/SmartHomeApi/releases/download/1.4.0-alpha-8/1.4.0-alpha-8_win-x86.zip">1.4.0-alpha-8 x86</a></div></div>
+          <div><a href="https://github.com/hdimon/SmartHomeApi/releases/download/1.4.0-alpha-10/1.4.0-alpha-10_win-x64.zip">1.4.0-alpha-10 x64</a></div>
+          <div><div><a href="https://github.com/hdimon/SmartHomeApi/releases/download/1.4.0-alpha-10/1.4.0-alpha-10_win-x86.zip">1.4.0-alpha-10 x86</a></div></div>
       </>
     ),
   }
